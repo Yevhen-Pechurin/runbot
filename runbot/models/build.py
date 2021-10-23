6cab0d4be165684047ec76611b1f1629f41eefb8
Yevhen Pechurin <yevhen.pechurin@gmail.com>
2021-10-23 19:44:25 +0300

    test 28 commit

--- runbot/models/build.py
+++ runbot/models/build.py
@@ -587,6 +587,7 @@ class BuildResult(models.Model):
                 continue
             build._run_job()
 
+
     def _process_requested_actions(self):
         for build in self:
             if build.requested_action == 'deathrow':
